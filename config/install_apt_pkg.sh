861fd497d77f89faaee4160bb6205610c6de3a7c
Miro Mlynarik <44208384+mmlynarik@users.noreply.github.com>
2022-10-03 17:27:31 +0200

    Update install_apt_pkg.sh

--- config/install_apt_pkg.sh
+++ config/install_apt_pkg.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
+HTTP_PROXY=
+HTTPS_PROXY=
+
 # APT
+sudo echo export Acquire::http::Proxy "${HTTP_PROXY}/"; >> /etc/apt/apt.conf.d/proxy.conf
+sudo echo export Acquire::https::Proxy "${HTTPS_PROXY}/"; >> /etc/apt/apt.conf.d/proxy.conf
+
 sudo apt update
 sudo apt -y upgrade
 sudo apt install -y python3.9
@@ -15,8 +21,8 @@ echo alias cl=clear >> ~/.bashrc
 echo alias jl=\"jupyter lab --no-browser --port 8888 --ip=\'127.0.0.1\' --ContentManager.allow_hidden=True --ServerApp.token=\'\' --ServerApp.password=\'\'\" >> ~/.bashrc
 echo alias nn=\"nano ~/.bashrc\" >> ~/.bashrc
 echo export PATH=\"\$HOME/.poetry/bin:\$PATH\" >> ~/.bashrc
-echo export HTTPS_PROXY=http://user:password@host:port >> ~/.bashrc
-echo export HTTP_PROXY=http://user:password@host:port >> ~/.bashrc
+echo export HTTPS_PROXY=${HTTP_PROXY} >> ~/.bashrc
+echo export HTTP_PROXY=${HTTP_PROXY} >> ~/.bashrc
 
 # POETRY
 curl -sSL https://install.python-poetry.org | python3.9 -
